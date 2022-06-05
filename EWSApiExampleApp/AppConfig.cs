608bd52cdef7e9cc80eb6688fcf3f1e34d7854c3
Oleg0392 <genri.kocsfa@gmail.com>
2022-06-05 21:34:54 +0500

    Initial commit

---
+++ EWSApiExampleApp/AppConfig.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Reflection;
+
+namespace EWSApiExampleApp
+{
+    class AppConfig
+    {
+        public string configPath;
+        public string parrentFolderPath;
+        public int exchangeVersion;
+        public string fileExtension;
+        public int webCredentialsType;
+        public string username;
+        public string password;
+        public string domain;
+        public string serviceUrl;
+        public string userEmailAddress;
+        public int viewPageSize;
+
+        //Config logs
+        public string LogExceptionMessage1;
+        public string LogFileWritingComplete;
+
+        //Regex XML patterns
+        private string mainPattern = @"<[A-z]+\d*>\S*.+:?</[A-z]+\d*>";
+        private string namePattern = @"<[A-z]+\d*>";
+        private string valuePattern = @">\S*.+:?<";
+
+        //Config dictionary
+        private string[] settingNames = new string[20];
+        private string[] settingValues = new string[20];
+
+        public AppConfig(string filePath)
+        {
+            configPath = filePath;
+            StreamReader streamReader;
+            int settingIndex = 0;
+
+            Regex regexMain = new Regex(mainPattern);
+            Regex regexSettingName = new Regex(namePattern);
+            Regex regexSettingValue = new Regex(valuePattern);
+
+            if (!(String.IsNullOrEmpty(configPath)))
+            {
+                try
+                {
+                    streamReader = new StreamReader(filePath);
+                    string configText = streamReader.ReadToEnd();
+
+                    MatchCollection matchCollection = regexMain.Matches(configText);
+                    foreach (Match match in matchCollection)
+                    {
+                        string currentItem = match.Value;
+                        string ItemName = regexSettingName.Match(currentItem).Value;
+                        string ItemValue = regexSettingValue.Match(currentItem).Value;
+
+                        settingNames[settingIndex] = ItemName.Substring(1, ItemName.Length - 2);
+                        settingValues[settingIndex] = ItemValue.Substring(1, ItemValue.Length - 2);
+
+                        settingIndex++;
+                    }
+                } catch (Exception e) { Console.WriteLine($"{e.Message} Source: {e.Source}"); }
+
+                //Parsing config fields with reflection tools         
+                Type ConfigType = typeof(AppConfig);
+                FieldInfo[] fieldInfos = ConfigType.GetFields(BindingFlags.Public | BindingFlags.Instance | BindingFlags.DeclaredOnly | BindingFlags.NonPublic);
+                foreach (FieldInfo currentField in fieldInfos)
+                {
+                    int needIndex = getSettingIndex(currentField.Name);
+                    if (needIndex == -1) continue;                                //Если данное поле не я вляется настройкой или оно private
+                    if (currentField.FieldType == needIndex.GetType())
+                    {
+                        currentField.SetValue(this, Convert.ToInt32(settingValues[needIndex]));
+                    }
+                    else currentField.SetValue(this, settingValues[needIndex]);
+                }
+
+            }
+            else
+            {
+                throw new ArgumentException(filePath,"Invalid path to the config file!");
+            }
+ 
+        }
+
+
+        private int getSettingIndex(string settingName)
+        {
+            int valueIndex = -1;
+
+            for (int i = 0; i < settingNames.Length; i++)
+            {
+                if (settingName.Equals(settingNames[i]))
+                {
+                    valueIndex = i;
+                }
+
+            }
+
+            //if (valueIndex == -1) throw new ArgumentException(settingName, "Setting name is incorrect or absent in to the settings array.");
+
+            return valueIndex;
+        }
+
+
+
+        //Parsing config settings without Reflection
+        /*parrentFolderPath = settingValues[getSettingValue("parrentFolderPath")];
+          exchangeVersion = Convert.ToInt32(settingValues[getSettingValue("exchangeVersion")]);
+          fileExtension = settingValues[getSettingValue("fileExtension")];
+          webCredentialsType = Convert.ToInt32(settingValues[getSettingValue("webCredentialsType")]);
+          if (webCredentialsType == 2)
+          {
+              username = settingValues[getSettingValue("username")];
+              password = settingValues[getSettingValue("password")];
+              domain = settingValues[getSettingValue("domain")];
+          }
+          serviceUrl = settingValues[getSettingValue("serviceUrl")];
+          userEmailAddress = settingValues[getSettingValue("userEmailAddress")];
+          viewPageSize = Convert.ToInt32(settingValues[getSettingValue("viewPageSize")]);
+
+          LogExceptionMessage1 = settingValues[getSettingValue("LogExceptionMessage1")];
+          //LogFileWritingComplete = settingValues[getSettingValue("LogFileWritingComplete")];*/
+    }
+}
---
+++ EWSApiExampleApp/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.IO;
+using Microsoft.Exchange.WebServices.Data;
+using System.Timers;
+
+namespace EWSApiExampleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Initializing application configuration...");
+            AppConfig appConfig = new AppConfig("appConfig.xml");
+
+            Console.WriteLine("Initializing Exchange Web Service...");
+            ExchangeService service = new ExchangeService(ExchangeVersion.Exchange2007_SP1);
+
+            if (appConfig.webCredentialsType == 1)
+            {
+                service.Credentials = new WebCredentials();   //default credentials (if programm in user machine)
+            }
+            else service.Credentials = new WebCredentials(appConfig.username, appConfig.password, appConfig.domain);
+            
+
+            service.Url = new Uri(appConfig.serviceUrl);
+            Console.WriteLine("Redirection Url Validation...");
+            Console.WriteLine($"Validation: {RedirectionUrlValidationCallback(service.Url.OriginalString)}");
+
+            /*service.TraceEnabled = true;
+            service.TraceFlags = TraceFlags.All;*/
+
+            EmailMessage emailMessage = null;
+            Console.WriteLine("Creating and preparing ItemView element...");
+            ItemView view = new ItemView(appConfig.viewPageSize);
+            view.PropertySet = new PropertySet(ItemSchema.Subject, ItemSchema.DateTimeReceived, EmailMessageSchema.IsRead);
+            view.Traversal = ItemTraversal.Shallow;
+            view.OrderBy.Add(ItemSchema.DateTimeReceived, SortDirection.Descending);
+
+            try
+            {
+                Console.WriteLine("Try to find elements in mailbox...");
+                FindItemsResults<Item> results = service.FindItems(new FolderId(WellKnownFolderName.Inbox, appConfig.userEmailAddress), view);
+                Console.WriteLine("Loading properties of finded elements...");
+                service.LoadPropertiesForItems(results.Items, PropertySet.FirstClassProperties);
+                emailMessage = results.Items[0] as EmailMessage;
+                
+                /*foreach (Item item in results)
+                {
+                    Console.WriteLine($"Message subject: {item.Subject}");
+                    //Console.WriteLine($"Message id is: {item.Id.ToString()}");
+                    if (item is EmailMessage)
+                    {
+                        emailMessage = item as EmailMessage;
+                        Console.WriteLine($"Is Read: {message.IsRead.ToString()}");
+                    }
+                }*/
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"{appConfig.LogExceptionMessage1} {e.Message}");
+            }
+
+
+
+            if (emailMessage is not null)
+            {
+                
+                string filePath = $"{appConfig.parrentFolderPath}{emailMessage.Subject}{appConfig.fileExtension}";
+                Console.WriteLine("Saving message body to: {0}",filePath);
+                FileStream fileStream = File.Create(filePath);
+                fileStream.Close();
+                StreamWriter streamWriter = new StreamWriter(filePath);
+                streamWriter.WriteLine(emailMessage.Body.Text);
+                streamWriter.Close();
+                Console.WriteLine("Message body is writted in file.");
+            }
+
+            /*EmailMessage emailMessage = new EmailMessage(service);
+            emailMessage.ToRecipients.Add("rparobot140@mmk.ru");
+            emailMessage.Subject = "testMessage";
+            emailMessage.Body = new MessageBody("Hello pridurchatyi durachek, etoya, EWS API)))");
+            emailMessage.Send();*/
+
+            Console.ReadLine();
+            
+
+        }
+
+
+        private static bool RedirectionUrlValidationCallback(string redirectionUrl)
+        {
+            bool result = false;
+
+            Uri redirectionUri = new Uri(redirectionUrl);
+            if (redirectionUri.Scheme == "https")
+            {
+                result = true;
+            }
+
+            return result;
+        }
+    }
+
+
+}
